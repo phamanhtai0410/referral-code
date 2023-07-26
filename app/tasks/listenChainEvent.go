d5fc0fae20605ccdc2af9f12cd115dac3e3e1338
kawasaki <iduchungho@gmail.com>
2023-07-26 17:30:38 +0700

    edit task

--- app/tasks/listenChainEvent.go
+++ app/tasks/listenChainEvent.go
@@ -1,14 +1,14 @@
 package tasks
 
 import (
+	"log"
+	"time"
+
 	"example.com/refcode/v1/app/models"
 	"example.com/refcode/v1/app/models/contract"
 	"example.com/refcode/v1/pkg/configs"
 	"example.com/refcode/v1/platform/blockchain"
 	"github.com/ethereum/go-ethereum/common"
-	"log"
-	"math/big"
-	"time"
 )
 
 func ListenChainEvent() {
@@ -18,20 +18,20 @@ func ListenChainEvent() {
 		log.Fatal("WATCH WITHDRAW ERROR #1 ", err)
 	}
 	logs := make(chan *contract.RefCodeWithdrawn)
-	sub, err := refCode.WatchWithdrawn(nil, logs)
+	sub, _ := refCode.WatchWithdrawn(nil, logs)
 	for {
 		select {
 		case err = <-sub.Err():
 			log.Fatal("WATCH WITHDRAW ERROR #2: ", err)
 		case vlog := <-logs:
-			amount := new(big.Int).Quo(vlog.Amount, blockchain.EtherConst)
+			amount := vlog.Amount
 			log.Println("WITHDRAW EVENTS")
 			log.Printf("TO ADDRESS %s\n", vlog.Account.Hex())
 			log.Printf("AMOUNT %d\n", amount.Int64())
 			log.Printf("REMAINING %s\n", vlog.Remaining.String())
 			history := models.WithdrawHistory{
 				Address: vlog.Account.Hex(),
-				Amount:  float64(amount.Int64() / 1e18),
+				Amount:  float64(amount.Int64() / blockchain.EtherConst.Int64()),
 				Created: time.Now().UTC(),
 			}
 			err = history.Save()
