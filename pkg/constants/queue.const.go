af19c42895b2031d83d405d313b1d0fc3c05a930
kawasaki <iduchungho@gmail.com>
2023-07-28 18:40:00 +0700

    edit queue

--- pkg/constants/queue.const.go
+++ pkg/constants/queue.const.go
@@ -1,8 +1,8 @@
 package constants
 
 const (
-	// WorkerQueue          string = "worker.refcode_queue_prod"
-	WorkerQueue        string = "worker.refcode_queue_test"
+	WorkerQueue          string = "worker.refcode_queue_prod"
+	// WorkerQueue        string = "worker.refcode_queue_test"
 	MsgSaveUserDetail  string = "SAVE_USER_DETAIL"
 	MsgSaveRefCodeUsed string = "SAVE_REFCODE_INFO"
 	MsgRefCodeCounter  string = "REFCODE_COUNTER"
