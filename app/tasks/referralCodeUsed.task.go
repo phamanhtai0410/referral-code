2c5e1f0414d609a5bee647edac3f10cd972bba7d
kawasaki <iduchungho@gmail.com>
2023-08-05 11:08:25 +0700

    rm sentry

--- app/tasks/referralCodeUsed.task.go
+++ app/tasks/referralCodeUsed.task.go
@@ -1,12 +1,10 @@
 package tasks
 
 import (
-	"fmt"
 	"time"
 
 	"example.com/refcode/v1/app/models"
 	"example.com/refcode/v1/pkg/constants"
-	"example.com/refcode/v1/platform/sentry"
 	"github.com/RichardKnop/machinery/v1/log"
 )
 
@@ -20,6 +18,6 @@ func SaveRefCodeUsed(referralCode, domain, address, transactionHash string, pric
 		TransactionHash: transactionHash,
 	}
 	log.DEBUG.Println(constants.LogWorkerSaveCodeUsed+" INSERTED OK: ", model)
-	sentry.CaptureMessage(fmt.Sprintf("%s INSERTED OK: %s-%s-%s-%f", constants.LogWorkerSaveCodeUsed, model.Address, model.Domain, model.ReferralCode, model.Price))
+	// sentry.CaptureMessage(fmt.Sprintf("%s INSERTED OK: %s-%s-%s-%f", constants.LogWorkerSaveCodeUsed, model.Address, model.Domain, model.ReferralCode, model.Price))
 	return model.Save()
 }
